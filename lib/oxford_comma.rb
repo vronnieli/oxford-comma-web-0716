3516c17d25c929f706454ce73b5893058d6dfe7f
Veronica Li <vronnieli@gmail.com>
2016-07-27 02:51:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_item = array.pop
+    string = array.join(", ")
+    string << ", and #{last_item}"
+  end
 end
